246c29eacb337da05f3d9fe69d9d17811561c096
Unnamed_DEV <a22yo22io@gmail.com>
2023-02-28 19:55:02 +0100

    Added basic tab switching setup

--- Hexapol War V2/Assets/Scripts/MainMenu/MainMenuManager.cs
+++ Hexapol War V2/Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -5,11 +5,107 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
-    public void PlayOnline()
+    [SerializeField] GameObject mainTab;
+    [SerializeField] GameObject statsTab;
+    [SerializeField] GameObject settingsTab;
+
+    List<GameObject> tabs;
+
+    public enum menuState
     {
-        SceneManager.LoadScene("GameField");
+        MeinTab,
+        PlayOnline,
+        PlayOffline,
+        StatsTab,
+        SettingsTab,
+        Back,
+        Quit
     }
 
+    private void Awake()
+    {
+        //Add tabs to tabs list
+        tabs.Add(mainTab);
+        tabs.Add(statsTab);
+        tabs.Add(settingsTab);
+    }
+
+    //Switch tabs or do action when button is pressed
+    public void SwitchMenuState(menuState state)
+    {
+        CloseAllTabs();
+
+        switch (state)
+        {
+            case menuState.MeinTab:
+                break;
+
+            case menuState.PlayOnline:
+                SceneManager.LoadScene("GameField");
+                break;
+
+            case menuState.PlayOffline:
+                SceneManager.LoadScene("GameField");
+                break;
+
+            case menuState.StatsTab:
+                statsTab.SetActive(true);
+                break;
+
+            case menuState.SettingsTab:
+                settingsTab.SetActive(true);
+                break;
+
+            case menuState.Back:
+                mainTab.SetActive(true);
+                break;
+
+            case menuState.Quit:
+                break;
+        }
+    }
+
+    //Helpfull functions
+
+    //Close all tabs
+    public void CloseAllTabs()
+    {
+        foreach (GameObject menu in tabs)
+        {
+            menu.SetActive(false);
+        }
+    }
+
+
+
+    //Events
+
+    //Start games
+    public void PlayOnline()
+    {
+        SwitchMenuState(menuState.PlayOnline);
+    }
+    public void PlayOffline()
+    {
+        SwitchMenuState(menuState.PlayOffline);
+    }
+
+
+    //Open tabs
+    public void OpenStats()
+    {
+        SwitchMenuState(menuState.StatsTab);
+    }
+    public void OpenSettings()
+    {
+        SwitchMenuState(menuState.SettingsTab);
+    }
+    public void Back()
+    {
+        SwitchMenuState(menuState.Back);
+    }
+
+    //Leave game
     public void Quit()
     {
         Application.Quit();
