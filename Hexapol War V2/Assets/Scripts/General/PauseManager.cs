1a1724a3881bd7be32cff7c9808fc4cc4b763a65
Unnamed_DEV <a22yo22io@gmail.com>
2023-04-08 21:05:24 +0200

    Added Pause menu Functions

---
+++ Hexapol War V2/Assets/Scripts/General/PauseManager.cs
@@ -0,0 +1,66 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour
+{
+    public GameObject pauseMenu;
+
+    bool pauseMenuOpen = false;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!pauseMenuOpen)
+            {
+                OpenPauseMenu();
+            }
+            else
+            {
+                ClosePauseMenu();
+            }
+        }
+    }
+
+    public void OpenPauseMenu()
+    {
+        pauseMenuOpen = true;
+
+        pauseMenu.SetActive(true);
+
+        foreach (PlayerInteractions player in FindObjectsOfType<PlayerInteractions>())
+        {
+            player.enabled = false;
+        }
+    }
+
+    public void ClosePauseMenu()
+    {
+        pauseMenuOpen = false;
+
+        pauseMenu.SetActive(false);
+
+        foreach (PlayerInteractions player in FindObjectsOfType<PlayerInteractions>())
+        {
+            player.enabled = true;
+        }
+    }
+
+    public void LeaveServer()
+    {
+        NetworkManager.singleton.StopHost();
+        NetworkManager.singleton.StopClient();
+
+        Destroy(NetworkManager.singleton.gameObject);
+
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
